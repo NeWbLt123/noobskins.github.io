6063460e63d0f098106b1214509b78edb54d5d14
Antoine Cloutier <cloutier.antoine@gmail.com>
2015-12-30 21:51:27 -0500

    test

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name noobskinsgithubioApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the noobskinsgithubioApp
+ */
+angular.module('noobskinsgithubioApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
